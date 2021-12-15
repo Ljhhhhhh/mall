2482da069d1bfc8499bbc875e1f6c62fc9377212
ljh <13216698987@163.com>
2021-12-15 23:25:21 +0800

    冲冲冲

---
+++ src/main/java/pi/mall/MallApplication.java
@@ -0,0 +1,15 @@
+package pi.mall;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("ltd.newbee.mall.dao") //添加 @Mapper 注解
+public class MallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MallApplication.class, args);
+	}
+
+}
