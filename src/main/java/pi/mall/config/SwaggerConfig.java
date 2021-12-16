4d4490da173e5613bd4fc0927130a1331e8d74a9
皮皮卢 <2322294987@qq.com>
2021-12-16 22:42:45 +0800

    配置swagger

--- src/main/java/pi/mall/config/SwaggerConfig.java
+++ src/main/java/pi/mall/config/SwaggerConfig.java
@@ -12,36 +12,23 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class Swagger2Config {
+public class SwaggerConfig {
+
   @Bean
-  public Docket createRestApi() {
+  public Docket api() {
     return new Docket(DocumentationType.SWAGGER_2)
-      // 指定构建api文档的详细信息的方法：apiInfo()
       .apiInfo(apiInfo())
       .select()
-      // 指定要生成api接口的包路径
       .apis(RequestHandlerSelectors.basePackage("pi.mall.controller"))
-      //使用了 @ApiOperation 注解的方法生成api接口文档
-      //.apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
       .paths(PathSelectors.any())
-      //可以根据url路径设置哪些请求加入文档，忽略哪些请求
       .build();
   }
 
-  /**
-   * 设置api文档的详细信息
-   */
   private ApiInfo apiInfo() {
     return new ApiInfoBuilder()
-      // 标题
-      .title("Spring Boot集成Swagger2")
-      // 接口描述
-      .description("swagger")
-      // 联系方式
-      .contact("微信公众号"+"全栈学习笔记"+"359076197@qq.com")
-      // 版本信息
+      .title("swagger-api文档")
+      .description("swagger文档 by 13")
       .version("1.0")
-      // 构建
       .build();
   }
 }
